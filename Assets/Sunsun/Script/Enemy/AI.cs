43d27da56d243166d4a37a6842fe07dd6a25fc9c
sunsunship0218 <b92218@gmail.com>
2024-05-06 23:19:26 +0800

    state machine animation bug

---
+++ Assets/Sunsun/Script/Enemy/AI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator anim;
+    public Transform player;
+    State currentState;
+    private void Start()
+    {
+        agent=this.GetComponent<NavMeshAgent>();
+        anim = this.GetComponent<Animator>();
+        currentState = new Idle(this.gameObject, agent, anim, player);
+    }
+
+    private void Update()
+    {
+        currentState = currentState.process();
+    }
+}
