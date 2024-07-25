7e67d4ac52c63e05077418728b7042bae177ca97
sunsunship0218 <b92218@gmail.com>
2024-07-26 02:56:56 +0800

    colision bug finished

--- Assets/Sunsun/Script/Combat/Weapon.cs
+++ Assets/Sunsun/Script/Combat/Weapon.cs
@@ -13,26 +13,17 @@ namespace NIMA.Combat
 
         private void Awake()
         {
-<<<<<<< Updated upstream
+
             triggerBox = weapon.GetComponent<BoxCollider>();
-          
-=======
-            triggerBox = weapon.GetComponent<BoxCollider>();        
->>>>>>> Stashed changes
+
         }
         void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Enemy")
             {              
-<<<<<<< Updated upstream
-                Debug.Log(gameManager.npc1HealthSystem.GetHealth());
-                gameManager.npc1HealthSystem.Damage(damage);
-=======
-                Debug.Log("ATK");
-                gameManager.npc1HealthSystem.Damage(damage);
-                Debug.Log(gameManager.npc1HealthSystem.GetHealth());
->>>>>>> Stashed changes
 
+                Debug.Log(gameManager.npc1HealthSystem.GetHealth());
+                gameManager.npc1HealthSystem.Damage(damage);
             }
         }
 
